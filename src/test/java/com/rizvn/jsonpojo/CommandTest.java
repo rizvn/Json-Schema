692580ad9bbe002dfa94c1cc7af3efe83a0e9594
riz <riz@rizvn.com>
2017-01-06 21:37:29 +0000

    added more validations

--- src/test/java/com/rizvn/jsonpojo/CommandTest.java
+++ src/test/java/com/rizvn/jsonpojo/CommandTest.java
@@ -2,10 +2,16 @@ package com.rizvn.jsonpojo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rizvan.json.Command;
+import com.rizvan.json.Student;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.io.File;
+import java.util.Set;
 
 /**
  * Created by Riz
@@ -22,6 +28,37 @@ public class CommandTest {
 
     Command supplierCommand =  mapper.readValue(new File("src/test/resources/GetSupplier.json"), Command.class);
     Assert.assertEquals(supplierCommand.getGetSupplier().getRequest().getCustomerRef(), "123");
+  }
+
+  @Test
+  public void testCommandBindingAndValidationSuccess() throws Exception
+  {
+    ObjectMapper mapper = new ObjectMapper();
+
+    Command cmd =  mapper.readValue(new File("src/test/resources/GetAddress.json"), Command.class);
+
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+
+    Set<ConstraintViolation<Command>> validationResults = validator.validate(cmd);
+
+    Assert.assertTrue("Expected no validation failures but found some",validationResults.isEmpty());
+  }
+
+  @Test
+  public void testCommandBindingAndValidationFail() throws Exception
+  {
+    ObjectMapper mapper = new ObjectMapper();
+
+    Command cmd =  mapper.readValue(new File("src/test/resources/GetSupplier.json"), Command.class);
+
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+
+    //5. validations
+    Set<ConstraintViolation<Command>> validationResults = validator.validate(cmd);
+
+    Assert.assertFalse("Expected no validation failures but found some",validationResults.isEmpty());
   }
 
 }
